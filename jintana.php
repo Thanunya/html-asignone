64087226a9333c1d670e2401dd17f904fd6c425d
Thanunya <76036829+Thanunya@users.noreply.github.com>
2020-12-21 19:25:30 +0700

    Add files via upload

---
+++ jintana.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="th">
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>เว็บแนะนำตัว</title>
+  </head>
+
+<style>
+body {
+  background-image: url('https://3.bp.blogspot.com/-QoIL7-im2t0/XENN9L0oFPI/AAAAAAAA0vw/K8BO0psIpcQFgvx_y-HiS50R-YTz9xwiQCLcBGAs/s1600/pastel-pic02.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  text-align: center;
+}
+</style>
+
+  <body>
+
+        <h1>ประวัติส่วนตัว</h1>
+<p>ชื่อ-นามสกุล : จินตนา ณ วิเชียร</p>
+<p>เพศ : หญิง    อายุ : 20 ปี</p>
+<p>รหัสนักศึกษา : 620710784</p>
+<p>กำลังศึกษา : สาขาวิทยาการข้อมูล คณะวิทยาศาสตร์ มหาวิทยาลัยศิลปากร</p>
+<p>ที่อยู่ : 188/211 ม.4 หมู่บ้านสุวรรณ-สุวรรณภูมิ ต.บางพลีใหญ่ อ.บางพลี จ.สมุทรปราการ 10540</p>
+<p>ข้อมูลการติดต่อ :  0631976581 </p>
+<a href="https://www.facebook.com/jintana.nawichian">Facebook : Jintana Na wichian
+<br>
+<br>
+<img src="https://www.flaticon.com/svg/static/icons/svg/124/124010.svg" width="100" height="100"</a>
+<br>
+<br>
+<a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox">E-mail : jiatana0108@gmail.com
+<br>
+<br>
+<img src="https://www.google.com/gmail/about/static/images/logo-gmail.png?cache=1adba63" width="100" height="100"</a>
+
+  </body>
+</html>
